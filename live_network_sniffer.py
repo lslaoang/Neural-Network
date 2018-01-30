358b5c640db6201fce40c4858f4421a727d670b2
lslaoang <laurellaoang@gmail.com>
2018-01-30 16:20:17 +0800

    Protocol text added for additional libraries

---
+++ live_network_sniffer.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 30 16:02:13 2018
+
+@author: Lao
+"""
+
+import pyshark
--- network_sniffer.py
+++ network_sniffer.py
@@ -3,6 +3,7 @@ import sys
 import struct
 import re
 
+
 def receiveData(s):
     data = ''
     try:
